19ae4eac7d05d23e0b8d40b29cf70c56e0b24aaa
Avishai Ish-Shalom <avishai@fewbytes.com>
2013-08-03 18:48:49 +0300

    fixed wrong topologies file name, include both public and private addresses for same DC nodes

--- recipes/snitch.rb
+++ recipes/snitch.rb
@@ -7,10 +7,14 @@ file ::File.join(node["cassandra"]["conf_dir"], "cassandra-rackdc.properties") d
 	content node["cassandra"]["topology"].to_hash.map{|k,v| "#{k}=#{v}" }.join("\n")
 end
 
-topologies_content = cassandra_nodes.map{|n| 
+topologies_content = (cassandra_nodes.map{|n| 
 		"#{ip_for_node(n)}=#{n["cassandra"]["topology"]["dc"]}:#{n["cassandra"]["topology"]["rack"]}" 
-		}.join("\n")
-file ::File.join(node["cassandra"]["conf_dir"], "cassandra-topologies.properties") do
+		} + \
+		cassandra_nodes.select{|n| n["cassandra"]["dc"] == node["cassandra"]["dc"] and n["cloud"]["public_ipv4"]}.map{|n| 
+		"#{n["cloud"]["public_ipv4"]}=#{n["cassandra"]["topology"]["dc"]}:#{n["cassandra"]["topology"]["rack"]}" 
+		}
+		).uniq.sort.join("\n")
+file ::File.join(node["cassandra"]["conf_dir"], "cassandra-topology.properties") do
 	mode "0644"
 	content topologies_content
 end
